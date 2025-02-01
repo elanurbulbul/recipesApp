543333b0f7edcc1148b29a31486005afa36445a3
elanurbulbul <elanrbulbul@gmail.com>
2025-02-01 23:54:25 +0300

    CreatedSearchBar

--- src/components/common/Searchbar.jsx
+++ src/components/common/Searchbar.jsx
@@ -1,7 +1,13 @@
-
+import { BsSearch } from 'react-icons/bs'
 const Searchbar = () => {
   return (
-    <div>Searchbar</div>
+    <form className="search-bar">
+      <input type="text" id="query" name="query" placeholder="Search recipe here..." />
+      <button type="submit" className="search-btn" >
+        <BsSearch className="text-white" size={16} />
+      </button>
+    </form>
+
   )
 }
 
